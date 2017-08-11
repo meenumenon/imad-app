e6d6e159d2bafde16727963ef8e5ab18ffb05fdf
meenumenon <30681850+meenumenon@users.noreply.github.com>
2017-08-11 18:51:20 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/:articleName',function(req,res) {
    // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
    var articleName = req.params.articleName;
-   res.send(createTemplate(articles(articleName)));
+   res.send(createTemplate(articles[articleName]));
 });
 
 /*app.get('/article-two',function(req,res){
