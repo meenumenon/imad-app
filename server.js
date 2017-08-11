49813547541eeecfffb1e2bf032b96df0d6d522c
meenumenon <30681850+meenumenon@users.noreply.github.com>
2017-08-11 18:32:56 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var artice_one = {
+    
+    title : 'Article One',
+    heading  : 'Currently Reading',
+    content : ` <p>
+                   The Epic Measures
+                </p>
+                <p>
+                   The Sapiens
+                </p>
+                <p>
+                   Martin Luther King : Bearing the cross
+                </p>`
+    
+};
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function createTemplate (data){
+    
+     var title = data.title;
+     var heading = data.heading;
+     var content = data.content;
+    
+     var htmlTemplate = 
+    
+     `  <html>
+        <head>
+          <title>
+             ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </head>
+        <body>
+          <div>
+            <a href = "/">Home</a>
+          </div>
+           <hr/>
+          <h3>
+            ${heading}
+          </h3>
+          <div>
+            ${content}
+          </div>
+        </body>
+        </html>`;
+        
+        return htmlTemplate;
+    
+}
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -18,7 +70,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(artice_one));
 });
 
 app.get('/article-two',function(req,res){
