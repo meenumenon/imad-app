6405ae183f4045c6a434b0e963f8d12287155b9b
meenumenon <30681850+meenumenon@users.noreply.github.com>
2017-08-15 17:02:34 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var ele = document.getElementById('main-text');
 ele.innerHTML = "Happy Learning!!!!!";
+
+var img = document.getElementById('img');
+img.onClick = function(){
+    
+}
