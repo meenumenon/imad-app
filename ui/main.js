859074506e03ea101ddf25afe4819159da7dbd45
meenumenon <30681850+meenumenon@users.noreply.github.com>
2017-08-16 11:35:06 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,13 @@ console.log('Loaded!');
 var ele = document.getElementById('main-text');
 ele.innerHTML = "Happy Learning!!!!!";
 
-var img = document.getElementById('madi');
-console.log(img);
+var img = document.getElementById('img');
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onClick = function(){
-    img.style.marginLeft ="500px";
+   var interval = setInterval(moveRight,100); 
 };
