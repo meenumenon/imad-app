4d2499b0507be995fcc5df90d9edeb65e0f56a5a
meenumenon <30681850+meenumenon@users.noreply.github.com>
2017-08-17 19:13:42 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/article-three',function(req,res){
 });*/
 
 var names = [];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name/name',function(req,res){
     
 var name = req.params.name;
 names.push(name);
